fee5433c02d4f704e7b8b240507d775a0ce5d9e5
bexzod1509 <bekki20050905@gmail.com>
2024-04-07 21:10:47 +0500

    homework

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
+import { RiDeleteBin6Line } from "react-icons/ri";
 function App() {
+  const [text, setText] = useState("");
+  const [data, setData] = useState([]);
+  const handleSubmit = (e) => {
+    if (!text.trim()) return alert("Invalid text");
+    e.preventDefault();
+    let user = {
+      text,
+    };
+    setData([...data, user]);
+    setText("");
+  };
+  let users = data?.map((u, inx) => {
+    return (
+      <div key={inx} className="list">
+        <p>{u.text}</p>
+        <RiDeleteBin6Line />
+      </div>
+    );
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <form onSubmit={handleSubmit} action="">
+        <input
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+          type="text"
+          required
+        />
+        <button type="submit">Add</button>
+      </form>
+      <div>{users}</div>
     </div>
   );
 }
